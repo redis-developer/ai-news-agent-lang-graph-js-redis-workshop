60661d63a3fa78e21fa1669ea7d927baf65411ad
Guy Royse <guy@guy.dev>
2026-03-19 12:01:50 -0400

    fix: use app-relative path for footer logo

--- code/web/src/panels/footer/RedisLogo.svelte
+++ code/web/src/panels/footer/RedisLogo.svelte
@@ -1,4 +1,3 @@
 <a href="https://redis.io" target="_blank" rel="noopener noreferrer" class="pr-2">
-  <img src="/redis-logo-hyper.svg" alt="Redis" class="h-5 cursor-pointer" />
+  <img src="redis-logo-hyper.svg" alt="Redis" class="h-5 cursor-pointer" />
 </a>
-
